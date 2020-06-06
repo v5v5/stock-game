3c8cf56c938b1f086a5f38c3a6c55361cf9da02c
vb <dvavkvadrate@gmail.com>
2020-06-06 14:41:51 +0300

    improved calculate_sets_sequence()

--- main.py
+++ main.py
@@ -4,6 +4,7 @@
 # http://export.finam.ru/export9.out?market=6&em=95&token=03AGdBq26wCFtnU2koW48zWpyij4m149u0Dz7zTyg0rjHp6GXg7uBgyfxe3_hhMh0MXT4HiWt2uUlH5Ip4_0b1spURQRGSr5CnFWfhtmeN6pjlUdZHgDllTrkFEal3KqV70O42reEpuR5mMHUx16onlfB2E_z446U2fsVP9ZTBpXq_GF48lqDv9kUvb7ji-HSHfCVc0DzBu7weCPm_yShHMqKJXWYdzSojnMNr8UwtYDNFSzDncb7rFplfxIPtMoVk9Umu-LMc3VyWe2Adkyjr0mJ6h4Kar1KXo6gKu6k33WJB4t_zkFQ8A2hXJVdmQGruGHN9d7E-QBF2GdpQ8gz0nAH6iGqveOAu1Ol73etKtyUC2dKEo2vueuVZQ-NEapr681hJugLI25aa&code=RTSI&apply=0&df=3&mf=5&yf=2019&from=03.06.2019&dt=4&mt=5&yt=2020&to=04.06.2020&p=7&f=RTSI_190603_200604_60&e=.csv&cn=RTSI&dtf=4&tmf=4&MSOR=0&mstime=on&mstimever=1&sep=3&sep2=1&datf=1&at=1
 
 import csv
+import numpy as np
 
 path_to_data_file = "./RTSI_190603_200604_60.csv"
 max_count_of_days = 1
@@ -50,7 +51,11 @@ def print_input_data(input_data):
 
 def alalysis_input_data_of_day(input_data_of_day):
     set_sequence = calculate_sets_sequence(input_data_of_day)
-    print(set_sequence)
+    for key, value in set_sequence.items():
+        print(key)
+        print(value[0])
+        print(value[1])
+    # print(set_sequence)
 
 def calculate_sets_sequence(input_data_of_day):
     data = input_data_of_day
@@ -68,12 +73,13 @@ def calculate_sets_sequence(input_data_of_day):
                         f'out{data[i_out_close][TIME]}'
                         )
                     
-                    set_sequence[set_name] = []
+                    set_sequence[set_name] = ([],[])
                     data_in = ((float(data[i_in_close][CLOSE]) - float(data[i_in_open][OPEN])) /
                         float(data[i_in_open][OPEN]))
                     data_out = ((float(data[i_out_close][CLOSE]) - float(data[i_out_open][OPEN])) /
                         float(data[i_out_open][OPEN]))
-                    set_sequence[set_name].append((data_in, data_out))
+                    set_sequence[set_name][0].append(data_in)
+                    set_sequence[set_name][1].append(data_out)
                     count += 1
     # print(count)
     return set_sequence
