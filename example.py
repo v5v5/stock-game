dc3c031958e1b0ec95f7b39a4e5f0b6f55607d8d
vb <dvavkvadrate@gmail.com>
2020-06-14 20:28:46 +0300

    added example.py

---
+++ example.py
@@ -0,0 +1,14 @@
+# from dataclasses import dataclass
+
+# @dataclass
+class Name:
+    ar = []
+    pass
+
+name0 = Name()
+name0.a = "asd"
+name0.ar.append(1)
+print(name0.ar)
+
+name1 = Name()
+print(name1.ar)
